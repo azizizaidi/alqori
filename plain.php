20415200a314c1b91849062e40c723e65671dd1d
azizizaidi <azizikuis@gmail.com>
2023-05-13 13:26:12 +0800

    delete plain.php

--- plain.php
+++
@@ -1,7 +0,0 @@
-5474rt-alquran online aq/f.a online aq
-hryr55-alquran online aq
-test2-alquran online aq/f.a online aq
-
-wan awatif bq347 - 5474rt(id=105)
-nor hawira bq473 - hryr55(id=104)
-sharipah aq251   - test2(id=108)
