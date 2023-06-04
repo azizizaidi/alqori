2bca6f5fcac4b8d379d1cd951ebf3e4e4e34a63a
azizizaidi <azizikuis@gmail.com>
2023-06-04 11:43:52 +0800

    add calculator

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,7 +74,7 @@
                           </div>
                        
                       </div>          
--->
+
                        
                      
                      @elseif(Auth::user()->roles->contains(2))
